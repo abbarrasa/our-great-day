e828a515be988b6e658e4391b7c8d639b9d3c7ab
Alberto Buitrago <abuitrago@estrategiasdeinversion.com>
2018-01-29 23:58:55 +0100

    Bootstrap notify style

--- src/AppBundle/Resources/public/js/plugins/bootstrap-notify.js
+++ src/AppBundle/Resources/public/js/plugins/bootstrap-notify.js
@@ -64,7 +64,6 @@
         onClose: null,
         onClosed: null,
         icon_type: 'class',
-        //template: '<div data-notify="container" class="col-xs-11 col-sm-4 alert alert-{0}" role="alert"><button type="button" aria-hidden="true" class="close" data-notify="dismiss">&times;</button><i data-notify="icon" class="material-icons"></i><span data-notify="title">{1}</span> <span data-notify="message">{2}</span><div class="progress" data-notify="progressbar"><div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div></div><a href="{3}" target="{4}" data-notify="url"></a></div>'
         template: '<div data-notify="container" class="alert alert-{0}" role="alert"><div class="container-fluid"><div class="alert-icon"><i data-notify="icon" class="material-icons"></i></div><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true"><i class="material-icons">clear</i></span></button><span data-notify="title">{1}</span><span data-notify="message">{2}</span><div class="progress" data-notify="progressbar"><div class="progress-bar progress-bar-{0}" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%;"></div></div><a href="{3}" target="{4}" data-notify="url"></a></div></div>'
     };
 
@@ -231,10 +230,7 @@
         },
         styleDismiss: function() {
             this.$ele.find('[data-dismiss="alert"]').css({
-                position: 'absolute',
-                right: '10px',
-                top: '50%',
-                marginTop: '-13px',
+                cursor: 'pointer',
                 zIndex: this.settings.z_index + 2
             });
         },
