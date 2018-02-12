8cbd5c443bcb87a5ba6564ae1314beeeccd3ff36
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-02-12 12:13:49 +0100

    Merging with landing branch

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -2,26 +2,35 @@
 namespace AppBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
-use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class MenuBuilder
 {
+    const TEXT			 = 'Nuestro gran día';
+    const HASHTAGS               = ['veroycarlos', 'boda'];
+    const URL_PATTERN_FACEBOOK   = 'https://www.facebook.com/sharer.php?u=%url%&t=%text%';
+    const URL_PATTERN_TWITTER    = 'https://twitter.com/intent/tweet?url=%url%&text=%text%&hashtags=%hashtags%';
+    const URL_PATTERN_GOOGLEPLUS = 'https://plus.google.com/share?url=%url%';
+	
     /** @var FactoryInterface */
     private $factory;
-
-    /** @var RequestStack */
-    private $requestStack;
+    /** @var UrlGeneratorInterface */
+    protected $router;
+    /** @var TranslatorInterface */
+    protected $translator;
 
     /**
      * MenuBuilder constructor.
      * @param FactoryInterface $factory
-     * @param RequestStack $requestStack
+     * @param UrlGeneratorInterface $router
+     * @param TranslatorInterface $translator
      */
-    public function __construct(FactoryInterface $factory, RequestStack $requestStack)
+    public function __construct(FactoryInterface $factory, UrlGeneratorInterface $router, TranslatorInterface $translator)
     {
-        $this->factory      = $factory;
-        $this->requestStack = $requestStack;
+        $this->factory    = $factory;
+        $this->router     = $router;
+        $this->translator = $translator;
     }
 
     public function createMainMenu()
@@ -31,34 +40,20 @@ class MenuBuilder
 
         $menu
             ->addChild('Home', ['route' => 'homepage'])
-            ->setAttribute('icon', 'home')
-            ->setAttribute('class', 'nav-item')
+            ->setAttributes(['icon' => 'home', 'class' => 'nav-item'])
             ->setLinkAttribute('class', 'nav-link')
-            ->setExtra('translation_domain', 'AppBundle')
-        ;
-
-        $menu
-            ->addChild('Contact us', ['uri' => $this->requestStack->getCurrentRequest()->getBasePath() . '#contact-us'])
-            ->setAttribute('icon', 'contact_mail')
-            ->setAttribute('class', 'nav-item')
-            ->setLinkAttribute('class', 'nav-link')
-            ->setExtra('translation_domain', 'AppBundle')
         ;
 
         $menu
             ->addChild('Confirm attendance', ['route' => 'guest'])
-            ->setAttribute('icon', 'assignment_turned_in')
-            ->setAttribute('class', 'nav-item')
+            ->setAttributes(['icon' => 'assignment_turned_in', 'class' => 'nav-item'])
             ->setLinkAttribute('class', 'nav-link')
-            ->setExtra('translation_domain', 'AppBundle')
         ;
 
         $menu
             ->addChild('Guestbook', ['route' => 'guestbook'])
-            ->setAttribute('icon', 'import_contacts')
-            ->setAttribute('class', 'nav-item')
+            ->setAttributes(['icon' => 'import_contacts', 'class' => 'nav-item')
             ->setLinkAttribute('class', 'nav-link')
-            ->setExtra('translation_domain', 'AppBundle')
         ;
 
 //        if ($this->authorizationChecker->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
@@ -95,8 +90,65 @@ class MenuBuilder
 
 	    //{{ knp_menu_render('main', {'template': ':menu:knp_menu.html.twig'})) }}
     }
-}
 
+    public function createSocialMenu()
+    {
+        $menu = $this->factory->createItem('root');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav navbar-left');
+        $menu
+            ->addChild('facebook', ['uri' => $this->generateSocialUrl(self::URL_PATTERN_FACEBOOK)])
+            ->setLabel('')
+            ->setAttribute('icon', 'fa fa-facebook-square')
+            ->setLinkAttributes([
+                'onclick' => "javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;",
+                'target' => '_blank',
+                'title' => $this->translator->trans('frontend.menu.share', ['%site%' => 'Facebook']),
+                'data-toggle' => 'tooltip',
+                'data-placement' => 'bottom'
+            ])
+        ;
+        $menu
+            ->addChild('twitter', ['uri' => $this->generateSocialUrl(self::URL_PATTERN_TWITTER)])
+            ->setLabel('')
+            ->setAttribute('icon', 'fa fa-twitter')
+            ->setLinkAttributes([
+                'onclick' => "javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;",
+                'target' => '_blank',
+                'title' => $this->translator->trans('frontend.menu.share', ['%site%' => 'Twitter']),
+                'data-toggle' => 'tooltip',
+                'data-placement' => 'bottom'
+            ])
+        ;
+        $menu
+            ->addChild('googleplus', ['uri' => $this->generateSocialUrl(self::URL_PATTERN_GOOGLEPLUS)])
+            ->setLabel('')
+            ->setAttribute('icon', 'fa fa-google-plus-square')
+            ->setLinkAttributes([
+                'onclick' => "javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=350,width=480');return false;",
+                'target' => '_blank',
+                'title' => $this->translator->trans('frontend.menu.share', ['%site%' => 'Google+']),
+                'data-toggle' => 'tooltip',
+                'data-placement' => 'bottom'
+            ])
+        ;
+        $menu
+            ->addChild('frontend.menu.contact_us', ['uri' => '#'])
+            ->setAttribute('icon', 'fa fa-envelope-o')
+            ->setLinkAttribute('class', 'contact-us')
+        ;
+	    
+        return $menu;
+    }
+
+    private function generateSocialUrl($pattern)
+    {
+        return strtr($pattern, [
+            '%url%' => $this->router->generate('homepage', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            '%text%' => urlencode(self::TEXT),
+            '%hashtags%' => urlencode(implode(',', self::HASHTAGS))
+        ]);
+    }
+}
 
 /*
 public_backup function createQuery($context = 'list')
