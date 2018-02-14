c159ccdb67ed6744cddbb16211a57e6e5b5d17dd
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-02-14 18:17:56 +0100

    Find guests by criteria

--- src/AppBundle/Repository/GuestRepository.php
+++ src/AppBundle/Repository/GuestRepository.php
@@ -10,4 +10,17 @@ namespace AppBundle\Repository;
  */
 class GuestRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function findByCriteria(array $criteria)
+    {
+        $queryBuilder = $this->createQueryBuilder('g')
+        foreach($criteria as $field => $value) {
+            if (!in_array($field, array('firstname', 'lastname', 'email')) {
+                throw new \Exception();
+            }
+                
+            $queryBuilder->andWhere($queryBuilder->expr()->like($field, $value))           
+        }
+        
+        return $queryBuilder->getQuery()->getResult();
+    }
 }
