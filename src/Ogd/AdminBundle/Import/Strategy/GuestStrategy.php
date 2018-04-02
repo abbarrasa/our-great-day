79ac79571b0bbdb34cf949b8446e2a095809d3f4
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-04-02 16:37:31 +0200

    Update GuestStrategy.php

--- src/Ogd/AdminBundle/Import/Strategy/GuestStrategy.php
+++ src/Ogd/AdminBundle/Import/Strategy/GuestStrategy.php
@@ -4,6 +4,13 @@ namespace AdminBundle\Import\Strategy;
 
 class GuestStrategy implements StrategyInterface
 {
+    private $admin;
+    
+    public function __construct(AbstractAdmin $admin)
+    {
+        $this->admin = $admin;
+    }
+    
     public function validateData(array $headers, array $data)
     {
         // TODO: Implement validateData() method.
@@ -11,7 +18,21 @@ class GuestStrategy implements StrategyInterface
 
     public function import(array $headers, array $data)
     {
-        // TODO: Implement import() method.
+        $count = 0;
+        foreach($rows as $row) {
+            $reflectionClass = new \ReflectionClass(Guest::class);
+            $object          = $reflectionClass->newInstanceArgs();
+            foreach($row as $index => $value) {
+                $property           = $headers[$index];
+                $reflectionProperty = $reflectionClass->getProperty($property);
+                $reflectionProperty->setAccessible(true);
+                $reflectionProperty->setValue($object, $value);
+            }
+            $this->admin->create($object);
+            $count++;
+        }
+        
+        return $count;
     }
 
 }
