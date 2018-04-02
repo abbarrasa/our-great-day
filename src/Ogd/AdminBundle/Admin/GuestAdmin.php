85a6a70da824ca1f865681a955c5a6a27d978504
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-04-02 12:20:21 +0200

    Update GuestAdmin.php

--- src/Ogd/AdminBundle/Admin/GuestAdmin.php
+++ src/Ogd/AdminBundle/Admin/GuestAdmin.php
@@ -9,7 +9,7 @@ use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Route\RouteCollection;
 use Sonata\UserBundle\Model\User;
 
-class GuestAdmin extends AbstractAdmin
+class GuestAdmin extends AbstractAdmin implements ImportInterface
 {
     protected function configureRoutes(RouteCollection $collection)
     {
