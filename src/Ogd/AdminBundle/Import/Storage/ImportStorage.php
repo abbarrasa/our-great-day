b88e7f1503a7d9714646174255d5ea43e84a3468
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-04-05 16:31:32 +0200

    Update ImportStorage.php

--- src/Ogd/AdminBundle/Import/Storage/ImportStorage.php
+++ src/Ogd/AdminBundle/Import/Storage/ImportStorage.php
@@ -5,7 +5,7 @@ namespace AdminBundle\Import\Storage;
 
 interface ImportStorage
 {
-    public function filter(array $rows, &$errors = array());
+    public function filter(array $rows, &$errors = null);
     
     public function update(array $row);    
     
