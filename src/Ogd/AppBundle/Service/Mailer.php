e9fe058d080d8d04bc740030e26c65e8835de195
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-04-18 13:37:54 +0200

    Update Mailer.php

--- src/Ogd/AppBundle/Service/Mailer.php
+++ src/Ogd/AppBundle/Service/Mailer.php
@@ -93,7 +93,7 @@ class Mailer implements MailerInterface
      */
     public function sendConfirmationEmailMessage(UserInterface $user)
     {
-        $url = $this->router->generate('fos_user_security_autologin', ['username' => $this->encryptor->decrypt($user->getUsername())], UrlGeneratorInterface::ABSOLUTE_URL);
+        $url = $this->router->generate('fos_user_security_autologin', ['username' => $this->encryptor->encrypt($user->getUsername())], UrlGeneratorInterface::ABSOLUTE_URL);
         $message = $this->getMessage(self::TEMPLATE_REGISTER_NOTIFICATION, [
             'user' => $user,
             'accessUrl' => $url
