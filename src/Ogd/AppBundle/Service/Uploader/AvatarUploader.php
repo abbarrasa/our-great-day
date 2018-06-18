edafce3d578362b2bd2e0768ab68ecd11c8f3290
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-06-18 14:30:37 +0200

    Update AvatarUploader.php

--- src/Ogd/AppBundle/Service/Uploader/AvatarUploader.php
+++ src/Ogd/AppBundle/Service/Uploader/AvatarUploader.php
@@ -4,4 +4,8 @@ namespace AppBundle\Service\Uploader;
 
 class AvatarUploader extends FileUploader
 {
+    public function __construct()
+    {
+        $this->targetDirectory = User::PICTURE_WEB_DIR;
+    }  
 }
