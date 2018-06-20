524c9af02db3edb9ad42f2b9a2b493d2c5d2a0c1
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-06-20 11:55:07 +0200

    Update FileUploadListener.php

--- src/Ogd/AppBundle/Event/Listener/FileUploadListener.php
+++ src/Ogd/AppBundle/Event/Listener/FileUploadListener.php
@@ -3,7 +3,6 @@
 namespace AppBundle\Event\Listener;
 
 use AdminBundle\Entity\Post;
-use AppBundle\Service\Uploader\FileUploaderInterface;
 use Application\Sonata\UserBundle\Entity\User;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Doctrine\ORM\Event\LifecycleEventArgs;
@@ -11,47 +10,75 @@ use Doctrine\ORM\Event\PreUpdateEventArgs;
 
 class FileUploadListener
 {
-    /** @var FileUploaderInterface */
-    private $uploader;
-
-    public function __construct(FileUploaderInterface $uploader)
-    {
-        $this->uploader = $uploader;
-    }
-
     public function prePersist(LifecycleEventArgs $args)
     {
         $entity = $args->getEntity();
+        
+        // This logic only works for FileUploadInterface entities
+        if (!$entity instanceof FileUploadInterface) {
+            return;
+        }        
 
         $this->uploadFile($entity);
     }
 
     public function preUpdate(PreUpdateEventArgs $args)
     {
+        // Retrieve Form as Entity
         $entity = $args->getEntity();
+        
+        // This logic only works for FileUploadInterface entities
+        if (!$entity instanceof FileUploadInterface) {
+            return;
+        }
 
+        // Check which fields were changes
+        $changes = $args->getEntityChangeSet();
+        
+        if ($entity instanceof User) {
+            if (array_key_exists('picture', $changes)) {
+                $previousFileName = $changes['picture'][0];
+                $pathPreviousFile = $entity->getUploadRootDir().'/'. $previousFilename;
+                
+                // Remove it
+                if(file_exists($pathPreviousFile)){
+                    unlink($pathPreviousFile);
+                }
+            }                
+        } else if ($entity instanceof Post) {
+            if (array_key_exists('pictureCover', $changes)) {
+                $previousFileName = $changes['pictureCover'][0];
+                $pathPreviousFile = $entity->getUploadRootDir().'/'. $previousFilename;
+                
+                // Remove it
+                if(file_exists($pathPreviousFile)){
+                    unlink($pathPreviousFile);
+                }
+            }            
+        }
+                    
+        // Upload new file
         $this->uploadFile($entity);
     }
 
     private function uploadFile($entity)
     {
-        // upload only works for User and Post entities
         if ($entity instanceof User) {
             $file = $entity->getPicture();
-            // only upload new files
-            if ($file instanceof UploadedFile) {
-                $fileName = $this->uploader->upload($file);
-                $entity->setPicture($fileName);
-            }
         } else if ($entity instanceof Post) {
             $file = $entity->getCoverPicture();
-            // only upload new files
-            if ($file instanceof UploadedFile) {
-                $fileName = $this->uploader->upload($file);
-                $entity->setCoverPicture($fileName);
-            }
         }
-
-        return;
+        
+        // only upload new files
+        if ($file instanceof UploadedFile) {
+            $uploader = new FileUploader($entity->getUploadRootDir());
+            $fileName = $uploader->upload($file);
+        }
+        
+        if ($entity instanceof User) {
+            $entity->setPicture($fileName);            
+        } else if ($entity instanceof Post) {
+            $entity->setCoverPicture($fileName);
+        }
     }
 }
