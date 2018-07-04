4480cd4c0f7c80817769f70b54ec204235b1519d
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-07-04 11:30:23 +0200

    Update VichImageTypeExtension.php

--- src/Ogd/AppBundle/Form/Extension/VichImageTypeExtension.php
+++ src/Ogd/AppBundle/Form/Extension/VichImageTypeExtension.php
@@ -29,21 +29,37 @@ class VichImageTypeExtension extends AbstractTypeExtension
     }
 
     /**
-     * @param FormView $view
-     * @param FormInterface $form
-     * @param array $options
+     * {@inheritdoc}
+     */    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver
+            ->setDefaults([
+                'delete_label' => 'form.label.delete',
+                'select_label' => 'form.label.select',
+                'change_label' => 'form.label.change'
+        ]);
+    }    
+
+    /**
+     * {@inheritdoc}
      */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        if ($options['image_uri'] && !$options['imagine_pattern']) {
-            $parentData = $form->getParent()->getData();
-            $imageUri   = null;
+        if ($options['image_uri']) {
+            $view->vars['delete_label'] = $options['delete_label'];            
+            $view->vars['select_label'] = $options['select_label'];
+            $view->vars['change_label'] = $options['change_label'];
+            if (!$options['imagine_pattern']) {
+                $parentData = $form->getParent()->getData();
+                $imageUri   = null;
 
-            if (null !== $parentData) {
-                $imageUri = $this->storage->resolvePath($parentData, $form->getName());
-            }
+                if (null !== $parentData) {
+                    $imageUri = $this->storage->resolvePath($parentData, $form->getName());
+                }
 
-            $view->vars['image_uri'] = $imageUri;
-        }
+                $view->vars['image_uri'] = $imageUri;
+            }            
+        } 
     }
 }
