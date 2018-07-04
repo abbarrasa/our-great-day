907aa34e4a875d89bca7ffb1b9c487ae1e2b448e
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-07-04 11:34:36 +0200

    Update AddUserPictureSubscriber.php

--- src/Application/FOS/UserBundle/Form/EventSubscriber/AddUserPictureSubscriber.php
+++ src/Application/FOS/UserBundle/Form/EventSubscriber/AddUserPictureSubscriber.php
@@ -2,14 +2,9 @@
 
 namespace Application\FOS\UserBundle\Form\EventSubscriber;
 
-//use Application\Sonata\UserBundle\Entity\User;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-//use Symfony\Component\Form\Extension\Core\Type\FileType;
-//use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
-//use Symfony\Component\HttpFoundation\File\File;
-//use Symfony\Component\Validator\Constraints\Image;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class AddUserPictureSubscriber implements EventSubscriberInterface
@@ -23,9 +18,7 @@ class AddUserPictureSubscriber implements EventSubscriberInterface
 
     public function onPreSetData(FormEvent $event)
     {
-        /** @var User $data */
-        //$data    = $event->getData();
-        $form    = $event->getForm();
+        $form = $event->getForm();
 
         $form
             ->add('pictureFile', VichImageType::class, [
@@ -34,24 +27,11 @@ class AddUserPictureSubscriber implements EventSubscriberInterface
                 'allow_delete' => true,
                 'download_uri' => false,
                 'image_uri' => true,
+                'delete_label' => 'form.picture.remove',
+                'select_label' => 'form.picture.select_image',
+                'change_label' => 'form.picture.change',
                 'translation_domain' => 'FOSUserBundle',
             ])
-//
-//
-//            ->add('picture', FileType::class, [
-//                'label'              => 'form.picture',
-//                'required'           => false,
-//                //'data'               => $data !== null && $data->getPicture() !== null ? new File($data->getAbsolutePath()) : null,
-//                'image_path_method'  => 'getAbsolutePath'
-//                'translation_domain' => 'FOSUserBundle',
-//                'constraints'        => [
-//                    new Image([
-//                        'maxWidth' => 180,
-//                        'maxHeight' => 180,
-//                        'maxSize'   => '800k'
-//                    ])
-//                ]
-//            ])
         ;
     }
 }
