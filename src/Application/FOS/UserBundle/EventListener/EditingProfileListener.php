cd94871126567538750455890c6b2e6109d7dbd7
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-07-09 12:51:30 +0200

    Update EditingProfileListener.php

--- src/Application/FOS/UserBundle/EventListener/EditingProfileListener.php
+++ src/Application/FOS/UserBundle/EventListener/EditingProfileListener.php
@@ -7,22 +7,25 @@ use FOS\UserBundle\FOSUserEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class EditingProfileListener implements EventSubscriberInterface
 {
-    /**
-     * @var UrlGeneratorInterface
-     */
+    /** @var UrlGeneratorInterface */
     private $router;
-
+    /** @var EntityManagerInterface */
+    private $em;
+    
     /**
      * ResettingListener constructor.
      *
      * @param UrlGeneratorInterface $router
+     * @param EntityManagerInterface $em
      */
-    public function __construct(UrlGeneratorInterface $router)
+    public function __construct(UrlGeneratorInterface $router, EntityManagerInterface $em)
     {
         $this->router = $router;
+        $this->em     = $em;
     }
 
     /**
@@ -41,7 +44,28 @@ class EditingProfileListener implements EventSubscriberInterface
      */
     public function onProfileEditSuccess(FormEvent $event)
     {
+        $form = $event->getForm();
+        if ($form->has('changeReferences') && $form->get('changeReferences')->getData() === true) {
+            $user = $form->getData();
+            $this->changeAllUsernameReferences();
+        }
+        
         $url = $this->router->generate('fos_user_profile_edit');
         $event->setResponse(new RedirectResponse($url));
+    }
+    
+    public function changeAllUsernameReferences($user)
+    {
+        $uow     = $this->em->getUnitOfWork();
+        $changes = $uow->getEntityChangeSet($user);
+        if (array_key_exists('username', $changes)) {
+            $comments = $this->em->getRepository(AbstractComment::class)->findBy(['user' => $user]);            
+            foreach($comments as $comment) {
+                if ($comment->getName() !== $changes['username'][1]) {
+                    $comment->setName($changes['username'][1]);
+                    $this->em->persist($comment);
+                }
+            }
+        }
     }
 }
