ccdc41f94e5896a8c3091149031944ecdaee92b5
Alberto Buitrago Barrasa <abbarrasa@gmail.com>
2018-07-25 12:13:46 +0200

    Update ImportStorage.php

--- src/Ogd/AdminBundle/Import/Storage/ImportStorage.php
+++ src/Ogd/AdminBundle/Import/Storage/ImportStorage.php
@@ -7,7 +7,7 @@ interface ImportStorageInterface
 {
     //public function filter($data, array &$errors = null);
     
-    public function store($data, array &$errors = null);
+    public function store($data, array &$errors = null, array $options = array());
     
     //public function updateRow(array $row);
     
